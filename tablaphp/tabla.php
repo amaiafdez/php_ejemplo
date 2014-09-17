656541aebd248c056244ec13402eebf78739ea05
amaiafdez <afernandezvi13dw@ikzubirimanteo.com>
2014-09-17 10:22:40 +0200

    anadir tabla

---
+++ tablaphp/tabla.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+	<?php 
+    	echo $_GET["numero"];
+    	#for ($i=0; $i < $num; $i++) { 
+    		
+    	#}
+     ?>
+	
+
+	<table>
+		<tr>
+			<td>
+				
+			</td>
+		</tr>
+	</table>
+    
+
+</body>
+</html>
