3941c2834dd88070d73a5e8cfa298ac7de610750
amaiafdez <afernandezvi13dw@ikzubirimanteo.com>
2014-09-17 11:27:48 +0200

    modificar tabla

--- tablaphp/tabla.php
+++ tablaphp/tabla.php
@@ -1,16 +1,16 @@
 <html>
 <body>
-	<?php 
-		$num = (integer) $_GET["numero"];
-    	echo $num;
-    	'<table>'
-
-	    	for ($i=0; $i < num; $i++) { 
-	    		'<tr>
-	    			<td>' . i . '</td>
-	    		</tr>'
+	
+    <table>
+		<?php 
+			$num = $_GET["numero"];
+	    	
+	    	for ($i=0; $i < $num; $i++) { 
+	    	echo'<tr>
+	    			<td>hh</td>
+	    		</tr>';
 	    	}
-	    '</table>'
-     ?>
+     	?>
+     </table>
 </body>
 </html>
